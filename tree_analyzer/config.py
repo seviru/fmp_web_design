fc47384d081740b69f7f297c6fe95fcc265ca6e5
seviru <sebastian.vruibal@gmail.com>
2020-07-06 18:35:54 +0200

    Calculus algorithms which cant differentiate gaps wont have that option available anymore to select

--- tree_analyzer/config.py
+++ tree_analyzer/config.py
@@ -5,7 +5,7 @@ web correct functioning.
 
 calculus_algorithms =  {"simple": {"fancy_name" : "Simple", "differentiate_gaps" : "Y"},
                         "all_vs_all": { "fancy_name" : "All vs All", "differentiate_gaps" : "Y"},
+                        "all_vs_all_means": { "fancy_name" : "All vs All (Means)", "differentiate_gaps" : "Y"},
                         "whole_annotation_simple": { "fancy_name" : "Simple (Whole annotation)", "differentiate_gaps" : "N"},
                         "whole_annotation_all_vs_all": { "fancy_name" : "All vs All (Whole annotation)", "differentiate_gaps" : "N"},
-                        "all_vs_all_means": { "fancy_name" : "All vs All (Means)", "differentiate_gaps" : "Y"},
                         "whole_annotation_all_vs_all_means": { "fancy_name" : "All vs All (Means + Whole annotation)", "differentiate_gaps" : "N"}}
