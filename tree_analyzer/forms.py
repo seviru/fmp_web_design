7ee26200bdac7e7dc22827b94873d3bd42873e85
seviru <sebastian.vruibal@gmail.com>
2020-07-13 22:11:03 +0200

    Implemented django sessions: now tree designing is WAY faster

--- tree_analyzer/forms.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from django import forms
-from .models import Post
-
-class ParameterForms(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ("calc_alg", "diff_gaps", "features", "evalue")
--- tree_analyzer/views.py
+++ tree_analyzer/views.py
@@ -5,8 +5,9 @@ from django.template import loader
 import sys
 from django.core.management import call_command
 sys.path.append("/home/biopeqqer/Desktop/fmp_core_functionality/scripts")
-from src import main_class
+from src import main_class, feature_processing
 from tree_analyzer import config
+import json
 
 BASE_DATA_PATH = "/home/biopeqqer/Desktop/fmp_web_design/tree_analyzer/data"
 
@@ -23,25 +24,39 @@ def index(request):
     template = loader.get_template("tree_analyzer/cluster_list.html")
     return HttpResponse(template.render({"cluster_list":cluster_dict, "calculus_algorithms":config.calculus_algorithms}, request))
 
-def design_tree(request, cluster_number, features="ALL", min_evalue=1e-100, calc_algorithm="simple", differentiate_gaps="Y"):
-    if request.method == "POST":
-        form_params = dict(request.POST)
-        features = ','.join([str(feat) for feat in form_params["features"]])
-        min_evalue = float(form_params["evalue"][0])
-        calc_algorithm = form_params["calc_alg"][0]
-        differentiate_gaps = form_params["diff_gaps"][0]
+def design_tree(request, cluster_number):   
 
     cluster_list_file = f"{BASE_DATA_PATH}/cluster_list.txt"
     with open(cluster_list_file, "r") as cluster_list:
         for line in cluster_list:
             if cluster_number in line:
                 partition = line.split("\t")[1].rstrip()
-    case_study = main_class.FeatureStudy(f"{BASE_DATA_PATH}/partitions/{partition}/trees/{cluster_number}.tree",
-                                   f"{BASE_DATA_PATH}/partitions/{partition}/alignments/{cluster_number}.fas.alg",
-                                   f"{BASE_DATA_PATH}/partitions/{partition}/tables/{cluster_number}.tsv",
-                                   f"{BASE_DATA_PATH}/uniprot_2018_09.json",
-                                   features, min_evalue, calc_algorithm, differentiate_gaps)
-    case_study.study_features=list(case_study.study_features)
-    template = loader.get_template("tree_analyzer/design_tree.html")
+
+    if request.method == "GET":
+        case_study = main_class.FeatureStudy(f"{BASE_DATA_PATH}/partitions/{partition}/trees/{cluster_number}.tree",
+                                    f"{BASE_DATA_PATH}/partitions/{partition}/alignments/{cluster_number}.fas.alg",
+                                    f"{BASE_DATA_PATH}/partitions/{partition}/tables/{cluster_number}.tsv",
+                                    f"{BASE_DATA_PATH}/uniprot_2018_09.json",
+                                    "ACT_SITE", 1e-10, "simple", "Y")
+        case_study.all_features = list(case_study.all_features)
+        request.session["case_study"] = case_study
+    else: # IF METHOD IS POST
+        case_study = request.session["case_study"]
+        form_params = dict(request.POST)
+        if form_params["calc_alg"][0] is not "":
+            case_study.calc_alg = form_params["calc_alg"][0]
+        if "features" in form_params:
+            case_study.study_features = set(form_params["features"])
+        if form_params["evalue"][0] is not "":
+            case_study.min_eval = float(form_params["evalue"][0])
+        if (config.calculus_algorithms[case_study.calc_alg]["differentiate_gaps"]) == "N":
+            case_study.differentiate_gaps = "N"
+        else:
+            if "diff_gaps" in form_params and form_params["diff_gaps"][0] is not "":
+                case_study.differentiate_gaps = form_params["diff_gaps"][0]
+        request.session["case_study"] = case_study
+
     case_study.design_tree()
-    return HttpResponse(template.render({"case_study":case_study, "cluster":cluster_number, "calculus_algorithms":config.calculus_algorithms}, request))
+
+    template = loader.get_template("tree_analyzer/design_tree.html")
+    return HttpResponse(template.render({"case_study":case_study, "cluster":cluster_number, "calculus_algorithms":config.calculus_algorithms, "feature_info":config.feature_info}, request))
