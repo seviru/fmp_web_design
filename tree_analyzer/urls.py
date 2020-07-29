9a3cf8729552f76691a99563ae03c0f2c49e3526
seviru <sebastian.vruibal@gmail.com>
2020-07-29 21:53:48 +0200

    Now the user can write manually the positions that wants to analyze

--- tree_analyzer/urls.py
+++ tree_analyzer/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("cluster_explorer", views.cluster_explorer, name="cluster_explorer"),
-    path("design_tree=custom", views.design_custom_tree, name="design_custom_tree"),
-    path("design_tree=<cluster_number>", views.design_tree, name="design_tree")
+    path("design_tree=custom/select_parameters", views.select_custom_parameters, name="select_custom_parameters"),
+    path("design_tree=custom/results", views.design_custom_tree, name="design_custom_tree"),
+    path("design_tree=custom/results/updated", views.update_custom_tree, name="update_custom_tree"),
+    path("design_tree=<cluster_number>", views.design_tree, name="design_tree"),
 ]
--- tree_analyzer/views.py
+++ tree_analyzer/views.py
@@ -20,6 +20,7 @@ def index(request):
     template = loader.get_template("tree_analyzer/index.html")
     return HttpResponse(template.render({"context":context}, request))
 
+
 def cluster_explorer(request):
     cluster_list_file = f"{BASE_DATA_PATH}/cluster_list.txt"
     cluster_dict = {}
@@ -30,6 +31,7 @@ def cluster_explorer(request):
     template = loader.get_template("tree_analyzer/cluster_explorer.html")
     return HttpResponse(template.render({"cluster_list":cluster_dict}, request))
 
+
 def design_tree(request, cluster_number):   
     cluster_list_file = f"{BASE_DATA_PATH}/cluster_list.txt"
     with open(cluster_list_file, "r") as cluster_list:
@@ -39,60 +41,66 @@ def design_tree(request, cluster_number):
 
     if request.method == "GET":
         case_study = main_class.FeatureStudy(tree_path=f"{BASE_DATA_PATH}/partitions/{partition}/trees/{cluster_number}.tree",
-                                    alignment_path=f"{BASE_DATA_PATH}/partitions/{partition}/alignments/{cluster_number}.fas.alg",
-                                    table_path=f"{BASE_DATA_PATH}/partitions/{partition}/tables/{cluster_number}.tsv",
-                                    uniprot_path=f"{BASE_DATA_PATH}/uniprot_2018_09.json",
-                                    annotation_features="ALL", min_evalue=1e-10, 
-                                    node_score_algorithm="simple", differentiate_gap_positions="Y")
+                                             alignment_path=f"{BASE_DATA_PATH}/partitions/{partition}/alignments/{cluster_number}.fas.alg",
+                                             table_path=f"{BASE_DATA_PATH}/partitions/{partition}/tables/{cluster_number}.tsv",
+                                             uniprot_path=f"{BASE_DATA_PATH}/uniprot_2018_09.json",
+                                             annotation_features="ALL", 
+                                             min_evalue=1e-10, 
+                                             node_score_algorithm="simple", 
+                                             differentiate_gap_positions="Y")
         case_study.all_features = list(case_study.all_features)
-        request.session["case_study"] = case_study
-
     else: # IF METHOD IS POST
         case_study = request.session["case_study"]
         update_params = dict(request.POST)
         case_study.update_features(update_params)
-        request.session["case_study"] = case_study
         
     case_study.design_tree()
-    ts = TreeStyle()
-    ts.layout_fn = lambda x: True
-    base64_img, img_map = case_study.processed_tree.render("%%return.PNG", 
-                                                            tree_style=ts)
-    case_study.processed_tree = base64_img.data().decode("utf-8")
+    case_study.processed_tree = case_study.etetree_to_image()
+
+    request.session["case_study"] = case_study
+
 
     template = loader.get_template("tree_analyzer/design_tree.html")
     return HttpResponse(template.render({"case_study":case_study, "cluster":cluster_number, "calculus_algorithms":config.calculus_algorithms, "feature_info":config.feature_info}, request))
 
 
 def select_custom_parameters(request):
+    template = loader.get_template("tree_analyzer/select_custom_parameters.html")
+    return HttpResponse(template.render({"calculus_algorithms":config.calculus_algorithms}, request))
 
-    return
+
 def design_custom_tree(request):
-    # if request.method == "GET":
-    #     pass
-    #     # Cargar la pagina para que salgan las pijdadas de los archivos:
-    # else: # SI EL METODO ES POST PORQUE YA SUBIO LOS ARCHIVOS
     form_params = dict(request.POST)
     form_files = dict(request.FILES)
+    calculus_algorithm = form_params["calc_alg"][0]
+    differentiate_gaps = form_params["diff_gaps"][0]
     tree_file = form_files["tree"][0]
     alignment_file = form_files["alignment"][0]
     
-    case_study = main_class.FeatureStudy(tree_path=tree_file,
-                            alignment_path=alignment_file,
-                            node_score_algorithm="simple", differentiate_gap_positions="Y",
-                            position_matrix=[20,40,60])
+    custom_case_study = main_class.FeatureStudy(tree_path=tree_file,
+                                                alignment_path=alignment_file,
+                                                node_score_algorithm=calculus_algorithm, 
+                                                differentiate_gap_positions=differentiate_gaps,
+                                                position_matrix=[20,40,60])
+                                         
     
-    case_study.tree_in = utils.bytefile_to_stringfile(tree_file)
-    case_study.align_in = utils.bytefile_to_stringfile(alignment_file)
+    custom_case_study.tree_in = utils.bytefile_to_stringfile(tree_file)
+    custom_case_study.align_in = utils.bytefile_to_stringfile(alignment_file)
+    custom_case_study.design_tree()
+    custom_case_study.processed_tree = custom_case_study.etetree_to_image()
+    request.session["custom_case_study"] = custom_case_study
 
-
-        # for line in treefile:
-        #     print(line)
-        # # print(case_study.tree_in)
-    case_study.design_tree()
-    print(case_study.node_scores)
-
-
-    context = "potato"
     template = loader.get_template("tree_analyzer/design_custom_tree.html")
-    return HttpResponse(template.render({"calculus_algorithms":config.calculus_algorithms}, request))
+    return HttpResponse(template.render({"custom_case_study":custom_case_study, "calculus_algorithms":config.calculus_algorithms}, request))
+
+
+def update_custom_tree(request):
+    custom_case_study = request.session["custom_case_study"]
+    update_params = dict(request.POST)
+    custom_case_study.update_features(update_params)
+    custom_case_study.design_tree()
+    custom_case_study.processed_tree = custom_case_study.etetree_to_image()
+    request.session["custom_case_study"] = custom_case_study
+
+    template = loader.get_template("tree_analyzer/design_custom_tree.html")
+    return HttpResponse(template.render({"custom_case_study":custom_case_study, "calculus_algorithms":config.calculus_algorithms}, request))
